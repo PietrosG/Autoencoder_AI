1e8b8de8bc880ee8c03c5ae3cecffc7bd4e2dcdd
PietrosG <gardzielp@gmail.com>
2025-05-16 19:15:50 +0200

    Update AI_Model.py

--- AI_Model.py
+++ AI_Model.py
@@ -9,7 +9,7 @@ y_test = to_categorical(y_test,100)
 x_train = x_train.astype('float32') / 255.0
 x_test = x_test.astype('float32') / 255.0
 
-#Neural Network variable 
+#Neural Network 
 cnn = Sequential([
                 layers.Conv2D(32, (3, 3),padding='same', activation='relu', input_shape=(32, 32, 3)),
                 layers.MaxPooling2D((2, 2)),
